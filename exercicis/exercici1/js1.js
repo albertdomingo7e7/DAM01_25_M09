207f3d8db30f705a10e461ed3a10690afd0549e4
albertdomingo7e7 <albert.domingo.7e7@itb.cat>
2025-12-12 11:18:53 +0100

    Add files via upload

---
+++ exercicis/exercici1/js1.js
@@ -0,0 +1,14 @@
+//EXERCICIS 1
+
+//Filtrar per a trobar l'element amb l'ID autor
+let verAutor = document.getElementById("autor").textContent
+console.log(verAutor)
+//Guardar el valor de h1 en una variable i mostrarlo
+let titulo = document.querySelector("h1").textContent
+console.log(titulo)
+//Espiar el input
+let saberInput = document.getElementById("input-comentario").value
+console.log(saberInput)
+
+
+
